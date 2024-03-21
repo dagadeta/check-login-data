97f5b67cef90b7ebfe827890cc22be6c73d4f44a
dagadeta <dagadeta@web.de>
2024-03-21 14:13:20 +0100

    Different password checking for different types of users

--- src/Main.kt
+++ src/Main.kt
@@ -3,25 +3,41 @@ fun main() {
     val password = readln()
     print("Please type your username: ")
     val username = readln()
+    print("Please type your type of user (1/2/3): ")
+    val usertype = readln()
+    val usertypeInt = usertype.toInt()
 
-    println("Password is valid: " + isValidPassword(password))
-    println("Password is " + checkPasswordStrength(password) + "% strong")
+    println("Password is valid: " + isValidPassword(password, usertypeInt))
 
     println("Username is valid: " + isValidUsername(username))
 }
 
-fun isValidPassword(password: String): Boolean {
+fun isValidPassword(password: String, usertype: Int): Boolean {
     var pwdlength = true
     var pwddigit = true
     var pwdupper = true
     var pwdlower = true
     var pwdspecial = true
 
-    if (password.length < 8) { pwdlength = false }
-    if (password.filter { it.isDigit() }.firstOrNull() == null) { pwddigit = false }
-    if (password.filter { it.isLetter() }.filter { it.isUpperCase() }.firstOrNull() == null) { pwdupper = false }
-    if (password.filter { it.isLetter() }.filter { it.isLowerCase() }.firstOrNull() == null) { pwdlower = false }
-    if (password.filter { !it.isLetterOrDigit() }.firstOrNull() == null) { pwdspecial = false }
+    if (usertype == 1) {
+        if (password.length < 8) { pwdlength = false }
+        if (countNumbers(password) < 1) {pwddigit = false}
+        if (countUpperCaseLetters(password) < 1) { pwdupper = false }
+        if (countLowerCaseLetters(password) < 1) { pwdlower = false }
+        if (countSpecialCharacters(password) < 1) { pwdspecial = false }
+    } else if (usertype == 2) {
+        if (password.length < 15) { pwdlength = false }
+        if (countNumbers(password) < 2) {pwddigit = false}
+        if (countUpperCaseLetters(password) < 2) { pwdupper = false }
+        if (countLowerCaseLetters(password) < 2) { pwdlower = false }
+        if (countSpecialCharacters(password) < 2) { pwdspecial = false }
+    } else if (usertype == 3) {
+        if (password.length < 25) { pwdlength = false }
+        if (countNumbers(password) < 4) {pwddigit = false}
+        if (countUpperCaseLetters(password) < 4) { pwdupper = false }
+        if (countLowerCaseLetters(password) < 4) { pwdlower = false }
+        if (countSpecialCharacters(password) < 4) { pwdspecial = false }
+    }
 
     println("")
     println("Password:")
@@ -38,10 +54,6 @@ fun isValidPassword(password: String): Boolean {
     }
 }
 
-fun checkPasswordStrength(password: String): Int {
-    return(100)
-}
-
 fun isValidUsername(username: String): Boolean {
     var usrnmlength = true
     var usrnmspecial = true
@@ -55,4 +67,44 @@ fun isValidUsername(username: String): Boolean {
     println("No special characters except for \".\", \"_\" and \"@\": $usrnmspecial")
 
     return usrnmlength && usrnmspecial
+}
+
+fun countNumbers(password: String): Int {
+    val regex = "\\d+".toRegex()
+    val matches = regex.findAll(password)
+    var count = 0
+    for (match in matches) {
+        count++
+    }
+    return count
+}
+
+fun countUpperCaseLetters(password: String): Int {
+    val regex = "[A-Z]".toRegex()
+    val matches = regex.findAll(password)
+    var count = 0
+    for (match in matches) {
+        count++
+    }
+    return count
+}
+
+fun countLowerCaseLetters(password: String): Int {
+    val regex = "[a-z]".toRegex()
+    val matches = regex.findAll(password)
+    var count = 0
+    for (match in matches) {
+        count++
+    }
+    return count
+}
+
+fun countSpecialCharacters(password: String): Int {
+    val regex = "[^A-Za-z0-9\\s]".toRegex()
+    val matches = regex.findAll(password)
+    var count = 0
+    for (match in matches) {
+        count++
+    }
+    return count
 }
